3745beb3384c2f39f56d34b9f6a0585f6da335c4
jalvarado9000 <jalvarado9000@gmail.com>
2022-12-18 05:30:35 -0800

    got more structure on the app

--- TwoSum/src/main/java/com/ltp/videogamerecords/frontend/App.java
+++ TwoSum/src/main/java/com/ltp/videogamerecords/frontend/App.java
@@ -16,38 +16,73 @@ public class App {
 
     public static void main(String[] args) {
 
-        String option = "";
+        char option = 'Y';
 
+    /*
+    Example:
+    nums = "[3,2,4]";
+    int target = 6;
+     */
         
 
     do{
-        Scanner in = new Scanner(System.in);
+    
+       Scanner in = new Scanner(System.in);
+       int selection = menuSelector();
+        
+        switch(selection){
+            case 1:{
+                TwoSum twoSum = calculateArrayTarget();
+                sendMoney(twoSum);
+                break;
+            }
+            case 2:printSendMoney();
+            break;
+            //case 3: deleteSendMoney();
+            case 4: option = continueWithProgram();
+            break;
+            default: System.out.println("Option was not found");
 
+        }
+
+       
+    }while(option == 'Y');
+  
+    }
+
+
+
+
+    public static char continueWithProgram(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Do you want to continue? Yes('Y') or No('N')");
+        char option = in.nextLine().toUpperCase().charAt(0);
+        return option;
+        
+    }
+
+    public static TwoSum calculateArrayTarget(){
+
+        Scanner in = new Scanner(System.in);
         System.out.println("Please write a array");
         String nums = "[" + in.nextLine() + "]";
         System.out.println("Please write the target");
         int target = in.nextInt();
+        return new TwoSum(nums,target);
 
-        TwoSum twoSum = new TwoSum(nums,target);
-
-        sendMoney(twoSum);
-        printSendMoney();
-
-        
-    }while(true);
-
-   // String nums = "[3,2,4]";
-
-    //int target = 6;
-
-
-    //TwoSum twoSum = new TwoSum(nums,target);
-
-   
-    
-  
     }
 
+    public static int menuSelector(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please select options");
+        System.out.println("Select 1 to calculate");
+        System.out.println("Select 2 print results");
+        System.out.println("Select 3 to delete results");
+        System.out.println("Select 4 to Exit Program");
+        int selection = in.nextInt();
+        return selection;
+
+    }
 
     
 
@@ -98,12 +133,7 @@ public class App {
     
     }
 
-
-
     }
-
-
-
 
 
     private static HttpEntity<TwoSum> makeTransferEntity(TwoSum twoSum) {
